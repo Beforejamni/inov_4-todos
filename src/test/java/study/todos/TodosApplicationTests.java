86ba7d5604c2ac3f9fcf6f6eaec406eef0c9bd06
Beforejamn1 <beforejamn1@gmail.com>
2025-10-28 14:43:49 +0900

    git init

---
+++ src/test/java/study/todos/TodosApplicationTests.java
@@ -0,0 +1,13 @@
+package study.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
